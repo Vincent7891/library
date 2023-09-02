83ab9528226a8882cefd5f0ded43dda47adc1e0b
Vincent7891 <137709573+Vincent7891@users.noreply.github.com>
2023-09-02 18:42:09 +0100

    refactor code to use classes

--- javascript.js
+++ javascript.js
@@ -1,57 +1,68 @@
-const myLibrary = [];
-
-function Book(title,author,pages,read) {
+class Book {
+    constructor(title, author, pages, read) {
     this.title = title 
     this.author = author
     this.pages = pages
-    this.read = read
+    this.read = read    
+    }
 }
 
-function addBookToLibrary(title,author,pages,read) {
-    const newBook = new Book(title,author,pages,read)
-    myLibrary.push(newBook)
-    addBookToUI(newBook); // Update the UI with the new book
-}
+class Library {
 
-function addBookToUI(book) {
-    const bookContainer = document.querySelector('.book-container');
-  
-    // Create a new book element
-    const bookElement = document.createElement('div');
-    bookElement.className = `book-element${myLibrary.indexOf(book)}`;
-  
-    // Populate the book element with details
-    bookElement.innerHTML = `
-      <h3> Title: ${book.title}</h3>
-      <p>Author: ${book.author}</p>
-      <p>Pages: ${book.pages}</p>
-      <button id = "book-read${myLibrary.indexOf(book)}">Read: ${book.read ? 'Yes' : 'No'}</button>
-      <button id="delete-button${myLibrary.indexOf(book)}"> Delete Book </button>`;
-    // Append the new book element to the book container
-    bookContainer.appendChild(bookElement);
-  }
+    constructor(library) {
+        this.myLibrary = []
+    }
 
-function deleteBook(){
-    document.querySelector(".book-container").addEventListener('click', function(e) {
-        for (let i = 0; i < myLibrary.length; i++) {
-            if (e.target.id === `delete-button${i}`) {
-                document.querySelector(`.book-element${i}`).remove();
+
+    addBookToLibrary(title,author,pages,read) {
+        const newBook = new Book(title,author,pages,read)
+        this.myLibrary.push(newBook)
+        this.addBookToUI(newBook); // Update the UI with the new book
+    }
+
+    addBookToUI(book) {
+        const bookContainer = document.querySelector('.book-container');
+    
+        // Create a new book element
+        const bookElement = document.createElement('div');
+        bookElement.className = `book-element${this.myLibrary.indexOf(book)}`;
+    
+        // Populate the book element with details
+        bookElement.innerHTML = `
+        <h3> Title: ${book.title}</h3>
+        <p>Author: ${book.author}</p>
+        <p>Pages: ${book.pages}</p>
+        <button id = "book-read${this.myLibrary.indexOf(book)}">Read: ${book.read ? 'Yes' : 'No'}</button>
+        <button id="delete-button${this.myLibrary.indexOf(book)}"> Delete Book </button>`;
+        // Append the new book element to the book container
+        bookContainer.appendChild(bookElement);
+    }
+
+    deleteBook(){
+        document.querySelector(".book-container").addEventListener('click', (e) => {
+            for (let i = 0; i < this.myLibrary.length; i++) {
+                if (e.target.id === `delete-button${i}`) {
+                    document.querySelector(`.book-element${i}`).remove();
+                }
             }
-        }
-    });
+        });
+    }
+
+    toggleRead(){
+        document.querySelector(".book-container").addEventListener('click', (e) => {
+            for (let i = 0; i < this.myLibrary.length; i++) {
+                if (e.target.id === `book-read${i}`) {
+                    this.myLibrary[i].read =! this.myLibrary[i].read;
+                    document.getElementById(`book-read${i}`).textContent = `Read: ${this.myLibrary[i].read ? 'Yes' : 'No'}`;
+                }
+            }
+        });
+    }
 }
 
-function toggleRead(){
-    document.querySelector(".book-container").addEventListener('click', function(e) {
-        for (let i = 0; i < myLibrary.length; i++) {
-            if (e.target.id === `book-read${i}`) {
-                myLibrary[i].read =! myLibrary[i].read;
-                document.getElementById(`book-read${i}`).textContent = `Read: ${myLibrary[i].read ? 'Yes' : 'No'}`;
-            }
-        }
-    });
-}
-  
+
+const myLibrary = new Library();
+
 function handleModalEvents(){
     const addBookButton = document.getElementById('addBookButton');
     const bookDialog = document.getElementById('bookDialog');
@@ -80,20 +91,14 @@ function formSubmission(){
         const pages = document.getElementById("pageNumber").value;
         const read = document.getElementById("readStatus").checked;
     
-        addBookToLibrary(title, author, pages, read);
+        myLibrary.addBookToLibrary(title, author, pages, read);
     
         // Clear the form
         event.target.reset();
     });
 }
 
-handleModalEvents()
-
-formSubmission()
-
-deleteBook()
-
-toggleRead()
-
-
-
+handleModalEvents();
+formSubmission();
+myLibrary.deleteBook();
+myLibrary.toggleRead();
